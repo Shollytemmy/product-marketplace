fda0439a3de9e9372ef1e425df7f2418341704de
Bunyameen <bunyameen@gmail.com>
2023-03-02 17:59:53 +0100

    feat: Router configuration added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Explore from './Pages/Explore'
+import ForgetPassword from './Pages/ForgetPassword'
+import NotFound from './Pages/NotFound'
+import Offers from './Pages/Offers'
+import Profile from './Pages/Profile'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
 
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="">
+    <>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Explore />} />
+        <Route path='offers' element={<Offers />} />
+        <Route path='profile' element={<SignIn />} />
+        <Route path='sign-in' element={<SignIn />} />
+        <Route path='sign-up' element={<SignUp />} />
+        <Route path='forget-password' element={<ForgetPassword />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </Router>
 
-    </div>
+    </>
   )
 }
 
